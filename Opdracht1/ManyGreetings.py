55d7a700f65aedb5e661c4445bbc7521902a4c6d
dtijsma <djurretijsma@hotmail.com>
2015-04-24 20:12:57 +0000

    Final greetings

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,3 +1,3 @@
-name=input('What is your name ? ')
-greetings=input('How many greetings ? ')
-print (greetings + ' x Hello, '+name+' !')
+name=input('What is your name? ')
+greetings=input('How many greetings? ')
+print (greetings + ' x Hello, '+name+'!')
