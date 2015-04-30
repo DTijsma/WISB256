16f6a07db7730e551033a0dd484db6207529ccdb
dtijsma <djurretijsma@hotmail.com>
2015-04-30 22:14:18 +0000

    Final push this night.

--- Opdracht2/CountPrimes.py
+++ Opdracht2/CountPrimes.py
@@ -1,7 +1,36 @@
 import sys
+import math
 
-file=open(sys.argv[1], "r")
+data=open(sys.argv[1], "r")
 
-print(file.read())
+C=0.6601618
 
-file.close()
+primelist=[]
+for line in data:
+    primelist.append(int(line.replace('\n', '')))
+
+data.close()
+
+piN=len(primelist)
+largestprime=primelist[piN-1]
+ratio=piN*(math.log(largestprime))/largestprime
+
+k=1
+piN2=0
+while k<piN:
+    if primelist[k]-primelist[k-1]==2:
+        piN2+=1
+    k+=1
+
+print('--------------------------------')
+print('Largest Prime =', largestprime)
+print('--------------------------------')
+print('pi(N)         =', piN )
+print('N/log(N)      =', largestprime/(math.log(largestprime)) )
+print('ratio         =', ratio )
+print('--------------------------------')
+print('pi_2(N)       =', piN2 )
+print('2CN/log(N)^2  =', 2*C*largestprime/(math.log(largestprime))**2 )
+print('ratio         =', piN2*((math.log(largestprime))**2)/(2*C*largestprime) )
+print('--------------------------------')
+
